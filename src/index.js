e7212d4bdaa37224caf5093a6508f50e690c474b
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-09-25 11:06:25 +0300

    25.09.2021/11:06

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './components/style/normalize.css'
+import App from './components/app/App.js'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
